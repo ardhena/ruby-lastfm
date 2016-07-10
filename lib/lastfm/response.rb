2c033fd98049924f757387f6654d57288a274256
olgad <olgadraber@gmail.com>
2016-07-11 00:25:00 +0200

    fix invalid xml response from api
    (for artist.search opensearch namespace declaration was missing)

--- lib/lastfm/response.rb
+++ lib/lastfm/response.rb
@@ -7,6 +7,8 @@ class Lastfm
 
     def initialize(body)
       @xml = XmlSimple.xml_in(body, 'ForceArray' => ['image', 'tag', 'user', 'event', 'correction'])
+    rescue REXML::UndefinedNamespaceException
+      @xml = XmlSimple.xml_in(body_with_opensearch_namespace(body), 'ForceArray' => ['image', 'tag', 'user', 'event', 'correction'])
     rescue REXML::ParseException
       @xml = XmlSimple.xml_in(body.encode(Encoding.find("ISO-8859-1"), :undef => :replace), 
                                           'ForceArray' => ['image', 'tag', 'user', 'event', 'correction'])
@@ -22,6 +24,19 @@ class Lastfm
 
     def error
       @xml['error']['code'].to_i
+    end
+
+    private
+
+    def body_with_opensearch_namespace(body)
+      tags = body.split(">")
+      invalid_tag_index = tags.find_index{ |t| t.match("results") }
+      tags[invalid_tag_index] += opensearch_namespace
+      tags.join(">")
+    end
+
+    def opensearch_namespace
+      " xmlns:opensearch=\"http://a9.com/-/spec/opensearch/1.1/\">"
     end
   end
 end
